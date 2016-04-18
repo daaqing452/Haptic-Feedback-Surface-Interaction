c249b72e197d04f66578aed5829fc1afb287eb1f
Yiqin Lu <daaqing452@gmail.com>
2016-04-18 17:16:21 +0800

    new tracking

---
+++ Tracking/Assets/Tracking.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using System.IO;
+
+public class Tracking : MonoBehaviour
+{
+    StreamReader reader;
+
+    // Use this for initialization
+    void Start()
+    {
+        reader = new StreamReader(new FileStream("Take 2016-04-15 03.02.24 PM.csv", FileMode.Open));
+        string line;
+        for (int i = 0; i < 7; i++)
+        {
+            line = reader.ReadLine();
+        }
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+        string line = reader.ReadLine();
+        string[] arr = line.Split(',');
+        int j = 0;
+        int bias = 2;
+        //for (int i = 0; i * 3 + 22 < arr.Length && j < 22; i++)
+        for (int i = 0; i * 3 + 2 < arr.Length && j < 12; i++)
+        {
+            GameObject sphere = GameObject.Find("Sphere (" + j.ToString() + ")");
+            /*Renderer r = sphere.GetComponent<Renderer>();
+            r.material.color = Color.blue;*/
+            if (arr[i * 3 + bias + 0] == "") continue;
+            j++;
+            float x = 10 * float.Parse(arr[i * 3 + bias + 0]) - sphere.transform.position.x;
+            float y = 10 * float.Parse(arr[i * 3 + bias + 1]) - sphere.transform.position.y;
+            float z = 10 * float.Parse(arr[i * 3 + bias + 2]) - sphere.transform.position.z;
+            sphere.transform.Translate(x, y, z, Space.World);
+        }
+    }
+}
+
+class Point3D
+{
+    float x;
+    float y;
+    float z;
+}
---
+++ Trackingx/Tracking/MainWindow.xaml.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.IO;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Tracking
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Main();
+        }
+
+        public void Main()
+        {
+            //new SampleReadFromFile(this);
+            new SampleFromNet();
+        }
+    }
+
+    class SampleReadFromFile
+    {
+        TrackingBase tracking = new TrackingNaive();
+        int framePerSecond = 2;
+
+        public SampleReadFromFile(MainWindow mainWindow)
+        {
+            TrackingBase.mainWindow = mainWindow;
+            Thread thread = new Thread(Main);
+            thread.IsBackground = true;
+            thread.Start();
+        }
+
+        public void Main()
+        {
+            Frame.rel = new List<KeyValuePair<int, int>>();
+            Frame.rel.Add(new KeyValuePair<int, int>(0, 1));
+            Frame.rel.Add(new KeyValuePair<int, int>(1, 2));
+            Frame.rel.Add(new KeyValuePair<int, int>(2, 3));
+            Frame.rel.Add(new KeyValuePair<int, int>(3, 4));
+
+            TrackingBase.GetFrameStd(new StreamReader(new FileStream("std.txt", FileMode.Open)));
+
+            StreamReader srTrack = new StreamReader(new FileStream("track.txt", FileMode.Open));
+            StreamWriter swRes = new StreamWriter(new FileStream("res.txt", FileMode.OpenOrCreate));
+            while (true)
+            {
+                Frame frame = TrackingBase.GetNextFrame(srTrack);
+                if (frame == null)
+                {
+                    break;
+                }
+                Frame frameTarget = tracking.Track(frame);
+                swRes.WriteLine("framestart");
+                for (int i = 0; i < frameTarget.pl.Count; ++i)
+                {
+                    swRes.WriteLine("p " + frameTarget.pl[i]);
+                }
+                swRes.WriteLine("frameend");
+                swRes.WriteLine("");
+                Thread.Sleep(1000 / framePerSecond);
+            }
+            srTrack.Close();
+            swRes.Close();
+        }
+    }
+
+    class SampleFromNet
+    {
+        TrackingBase Tracking = new TrackingNaive();
+        string mode = "console";
+        
+        public SampleFromNet()
+        {
+            TcpClient socket = new TcpClient();
+            socket.Connect("192.168.1.159", 7643);
+            StreamReader sr = new StreamReader(socket.GetStream());
+            StreamWriter sw = new StreamWriter(new FileStream("samp.txt", FileMode.OpenOrCreate));
+            while (true)
+            {
+                string line = sr.ReadLine();
+                if (line == null) break;
+                switch (mode)
+                {
+                    case "file":
+                        sw.WriteLine(line);
+                        break;
+                    case "console":
+                        Console.WriteLine(line);
+                        break;
+                }
+            }
+            socket.Close();
+            sr.Close();
+            sw.Close();
+        }
+    }
+}
